afb84475e4c4062b1c2db94c0c111b9aa693ea8c
wangzipeng <562207293@qq.com>
2019-05-28 18:38:52 +0800

    Initial submission

---
+++ app/src/main/java/com/example/wangzipeng/viewentryexiteffect/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wangzipeng.viewentryexiteffect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView;
+    }
+}
